0e423bfa1a640d8d9e4bf0138cac260542adecbd
Daniel Lyons <fusion@storytotell.org>
2017-06-13 23:41:50 -0600

    Revising the position tracking to go by boundaries
    This is rather than width and height. Width and height would be more
    useful if we were planning on moving objects around on screen, but since
    they'll be pretty much fixed in place it's probably a better use of our
    time and computational energy to just be aware of the width and height
    during generation.

--- loghack.pl
+++ loghack.pl
@@ -4,14 +4,13 @@
 %% use this operator for making points
 :- op(200, xfx, @).
 
-random_position(X@Y, W@H, RX@RY) :-
-    XMax is X + W, YMax is Y + H,
-    random_between(X, XMax, RX), random_between(Y, YMax, RY).
+random_position(L@T, R@B, X@Y) :-
+    random_between(L, R, X), random_between(T, B, Y).
 
-initial_state(state(player(PX@PY), Rooms)) :-
+initial_state(state(player(Player), Rooms)) :-
     generate_dungeon(Rooms),
-    random_member(room(X@Y, W@H, _), Rooms),
-    random_position(X@Y, W@H, PX@PY).
+    random_member(room(LT, BR, _), Rooms),
+    random_position(LT, BR, Player).
 
 main :- initial_state(State), main(State).
 
@@ -41,11 +40,11 @@ draw(state(Player, Rooms)) :-
 draw_player(player(X@Y)) :-
     format('~T~w~T', [goto(X,Y), '☻', goto(0,25)]).
 
-draw_room(room(X@Y,Width@Height,Lit)) :-
+draw_room(room(X@Y,XMax@YMax,Lit)) :-
+    Width is XMax - X,
     format(string(R1), '┌~`─t~*|┐', [Width, Width]),
     format('~T~w', [goto(X,Y), R1]),
     succ(Y,Y1),
-    YMax is Y+Height,
     forall(between(Y1,YMax,I),
            (format(string(RI), '│~`.t~*|│', [Width, Width]),
                format('~T~w', [goto(X,I), RI]))),
@@ -72,20 +71,16 @@ evaluate(player(X0@Y),  move(right), player(X1@Y))  :- succ(X0, X1).
 evaluate(player(X @Y1), move(up),    player(X @Y0)) :- succ(Y0, Y1).
 evaluate(player(X @Y0), move(down),  player(X @Y1)) :- succ(Y0, Y1).
 
-overlaps(room(X1@Y1, W1@H1, _), room(X3@Y3, W3@H3, _)) :-
-    X2 is X1 + W1, Y2 is Y1 + H1,
-    X4 is X3 + W3, Y4 is Y3 + H3,
+overlaps(room(X1@Y1, X2@Y2, _), room(X3@Y3, X4@Y4, _)) :-
     X3 =< X2, Y3 =< Y2, X1 =< X4, Y1 =< Y4.
 
 generate_dungeon(Rooms) :- generate_dungeon([], 10, Rooms).
 
-generate_room(room(X@Y,Width@Height,Lit)) :-
-    random_between(2,40,Width),
-    random_between(2,20,Height),
-    XMax is 80 - Width,
-    YMax is 25 - Height,
-    random_between(0,XMax,X),
-    random_between(0,YMax,Y),
+generate_room(room(L@T, R@B, Lit)) :-
+    random_between(2,40,L),
+    random_between(2,20,T),
+    random_between(L,60,R),
+    random_between(T,40,B),
     random_member(Lit, [lit,unlit]).
 
 generate_dungeon(Rooms, 0, Rooms).
